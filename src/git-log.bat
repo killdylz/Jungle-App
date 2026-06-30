e51187a2e2f42b63cb3f974b62ac8c33c87d6fd7
Dylan Rodrigues <dylanrodrigues2710@gmail.com>
2026-07-01 00:10:03 +0800

    Auto-deploy: 2026-07-01 00:10:03

--- src/git-log.bat
+++ src/git-log.bat
@@ -2,4 +2,5 @@
 cd /d "%~dp0.."
 git log --oneline -5 > "%~dp0git-log.txt" 2>&1
 git diff HEAD --stat >> "%~dp0git-log.txt" 2>&1
-echo === DONE === >> "%~dp0git-log.txt"
+git status --short >> "%~dp0git-log.txt" 2>&1
+echo === %time% DONE === >> "%~dp0git-log.txt"
