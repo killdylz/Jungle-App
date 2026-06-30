4197af46f65520f29e46387c45121a84f640aaa2
Dylan Rodrigues <dylanrodrigues2710@gmail.com>
2026-06-30 12:28:24 +0800

    Auto-deploy: 2026-06-30 12:28:24

--- src/push-me.bat
+++ src/push-me.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0.."
 git add -A
-git commit -m "Full redesign: all 8 screens rebuilt to match Canopy mockup"
+git commit -m "Add Music Hub, Schedule Board, Member Mobile, Brand Studio - Analytics upgraded"
 git push
 echo Done! Deploying now...
 pause
